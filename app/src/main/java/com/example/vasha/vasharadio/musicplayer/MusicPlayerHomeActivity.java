e69e92ef5ca4e7ba29dfdf24bcecf2009c8ed9e5
IveVasilj <ive.vasiljevic@gmail.com>
2018-09-13 21:12:34 +0100

    Project prepared for music player development

--- app/src/main/java/com/example/vasha/vasharadio/musicplayer/MusicPlayerHomeActivity.java
+++ app/src/main/java/com/example/vasha/vasharadio/musicplayer/MusicPlayerHomeActivity.java
@@ -2,14 +2,19 @@ package com.example.vasha.vasharadio.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.example.vasha.vasharadio.R;
 
 public class MusicPlayerHomeActivity extends AppCompatActivity {
-
+    ListView musicList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player_home);
+
+        musicList = (ListView)findViewById(R.id.musicList);
+
+        
     }
 }
